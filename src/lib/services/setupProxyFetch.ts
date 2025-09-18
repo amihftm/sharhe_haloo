db790ea7867cf57cf1b9d289f21b919f2dac191e
amih ftm <amihftm@gmail.com>
2025-09-18 16:39:53 +0330

    sucide

--- src/lib/services/setupProxyFetch.ts
+++ src/lib/services/setupProxyFetch.ts
@@ -1,16 +1,16 @@
-import fetch, { RequestInit } from 'node-fetch';
-import { HttpsProxyAgent } from 'https-proxy-agent';
+// import fetch, { RequestInit } from 'node-fetch';
+// import { HttpsProxyAgent } from 'https-proxy-agent';
 
-// Replace with your proxy URL
-const proxy = process.env.HTTPS_PROXY as string;
-const agent = new HttpsProxyAgent(proxy);
+// // Replace with your proxy URL
+// const proxy = process.env.HTTPS_PROXY as string;
+// const agent = new HttpsProxyAgent(proxy);
 
-const proxyFetch = (url: string | URL, init?: RequestInit) => {
-  return fetch(url, {
-    ...init,
-    agent, // this forces the request through the proxy
-  });
-};
+// const proxyFetch = (url: string | URL, init?: RequestInit) => {
+//   return fetch(url, {
+//     ...init,
+//     agent, // this forces the request through the proxy
+//   });
+// };
 
-// Patch global fetch
-(globalThis as any).fetch = proxyFetch;
+// // Patch global fetch
+// (globalThis as any).fetch = proxyFetch;
