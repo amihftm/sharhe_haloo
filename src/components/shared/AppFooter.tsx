64ff6916a870eee8dfcc9d5ff2cbddc6fddc74b1
amih ftm <amihftm@gmail.com>
2025-09-20 21:07:50 +0330

    fixed footer not stick to end

--- src/components/shared/AppFooter.tsx
+++ src/components/shared/AppFooter.tsx
@@ -2,7 +2,7 @@ export function AppFooter() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="sticky bottom-0 w-full border-t justify-items-center">
+    <footer className="absolute bottom-0 w-full border-t justify-items-center">
       <div className="container flex h-14 items-center justify-center ">
         <p className="text-sm text-muted-foreground ">
           &copy; {currentYear} شرح حالوو. تمامی حقوق محفوظ است.
